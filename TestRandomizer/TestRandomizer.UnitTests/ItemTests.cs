1920af382d22bc6442a8ce0b2ee1bcaa269ba0e9
Eugene Karashevich <evgkar@gmail.com>
2022-04-06 16:40:13 +0200

    Added tests for Item

---
+++ TestRandomizer/TestRandomizer.UnitTests/ItemTests.cs
@@ -0,0 +1,21 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace TestRandomizer.UnitTests
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void GivenNullItemId_WhenConstructing_ThenThrows()
+        {
+            // Arrange
+            string itemId = null;
+
+            // Act && Assert
+            var exception = Should.Throw<ArgumentNullException>(
+                () => new Item(itemId, ItemTypeEnum.Operational));
+
+            exception.ParamName.ShouldBe("itemId");
+        }
+    }
+}
